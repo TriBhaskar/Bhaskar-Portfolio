4cdc49d5306225a830551aed1ec5d48835714d40
TriBhaskar <panthribhaskar@gmail.com>
2024-03-09 22:54:32 +0530

    Remove commented out code and unused CSS styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header({ children }) {
   return (
     <header className="m-0 p-2 flex justify-center items-center">
       <div id="header-title" className="flex items-center gap-6">
-        <h1 className="text-3xl font-bold underline">HELLO BHASKAR</h1>
+        {/* <h1 className="text-3xl font-bold underline">HELLO BHASKAR</h1> */}
       </div>
       <nav>{children}</nav>
     </header>
