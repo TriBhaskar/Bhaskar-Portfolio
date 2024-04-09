f5ba95b946a5be25ac4186e2c4c202e70ceec5b3
TriBhaskar <panthribhaskar@gmail.com>
2024-04-09 15:47:01 +0530

    Refactor layout styles and add flexbox for Profile and Content components

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,3 +1,4 @@
+import projImg from "../assets/Waiau.png";
 export default function Content() {
   return (
     <main id="content" className="pt-24 lg:w-1/2 lg:py-24">
@@ -59,7 +60,55 @@ export default function Content() {
                 </header>
                 <div className="z-10 sm:col-span-6">
                   <h3 className="font-medium leading-snug text-slate-200">
-                    Junior Software Engineer
+                    Junior Software Engineer - Giesecke and Devrient
+                  </h3>
+                  <p className="mt-2 text-sm leading-normal">
+                    Build and maintain critical components used to construct
+                    Klaviyo’s frontend, across the whole product. Work closely
+                    with cross-functional teams, including developers,
+                    designers, and product managers, to implement and advocate
+                    for best practices in web accessibility.
+                  </p>
+                  <ul
+                    className="mt-2 flex flex-wrap"
+                    aria-label="Technologies used"
+                  >
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                        JavaScript
+                      </div>
+                    </li>
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                        TypeScript
+                      </div>
+                    </li>
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                        React
+                      </div>
+                    </li>
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                        Storybook
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </li>
+            <li className="mb-12">
+              <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-50">
+                <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                <header
+                  className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2"
+                  aria-label="2024 to Present"
+                >
+                  2024 — Present
+                </header>
+                <div className="z-10 sm:col-span-6">
+                  <h3 className="font-medium leading-snug text-slate-200">
+                    Junior Software Engineer - Giesecke and Devrient
                   </h3>
                   <p className="mt-2 text-sm leading-normal">
                     Build and maintain critical components used to construct
@@ -97,6 +146,105 @@ export default function Content() {
               </div>
             </li>
           </ol>
+          <div className="mt-12">
+            <a
+              className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 font-semibold text-slate-200 group/link text-base"
+              href="/resume.pdf"
+              target="_blank"
+              rel="noreferrer noopener"
+              aria-label="View Full Résumé (opens in a new tab)"
+            >
+              <span>
+                View Full{" "}
+                <span className="inline-block">
+                  Résumé
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </span>
+              </span>
+            </a>
+          </div>
+        </div>
+      </section>
+      {/* Projects */}
+      <section
+        className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+        aria-label="Projects"
+      >
+        <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+          <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
+            Projects
+          </h2>
+        </div>
+        <div>
+          <ul className="group/list">
+            <li className="mb-12">
+              <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                <div className="z-10 sm:order-2 sm:col-span-6">
+                  <h3>
+                    <a
+                      className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+                      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+                      target="_blank"
+                      rel="noreferrer noopener"
+                      aria-label="Build a Spotify Connected App (opens in a new tab)"
+                    >
+                      <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                      <span>
+                        Build a Spotify Connected
+                        <span className="inline-block">
+                          App
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                            aria-hidden="true"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                              clipRule="evenodd"
+                            ></path>
+                          </svg>
+                        </span>
+                      </span>
+                    </a>
+                  </h3>
+                  <p className="mt-2 text-sm leading-normal">
+                    Video course that teaches how to build a web app with the
+                    Spotify Web API. Topics covered include the principles of
+                    REST APIs, user auth flows, Node, Express, React, Styled
+                    Components, and more.
+                  </p>
+                </div>
+                <img
+                  alt="Build a Spotify Connected App Newline course marketing card"
+                  loading="lazy"
+                  width="200"
+                  height="48"
+                  decoding="async"
+                  data-nimg="1"
+                  className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+                  style={{ color: "transparent" }}
+                  // srcSet="../assets/Waiau.png&amp;w=256&amp;q=75 1x, ../assets/Waiau.png&amp;w=640&amp;q=75 2x"
+                  src={projImg}
+                ></img>
+              </div>
+            </li>
+          </ul>
         </div>
       </section>
     </main>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,9 +8,11 @@ export default function Layout() {
         className="w-screen min-h-screen bg-cover bg-center bg-no-repeat bg-blend-color-dodge"
         style={{ backgroundImage: `url(${bgImg})` }}
       >
-        <div className="mx-auto min-h-screen flex max-w-screen-xl px-6 py-12 md: px-12 md: py-20 lg: px-24 lg:py-0">
-          <Profile />
-          <Content />
+        <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md: px-12 md: py-20 lg: px-24 lg:py-0">
+          <div className="lg:flex lg:justify-between lg:gap-4">
+            <Profile />
+            <Content />
+          </div>
         </div>
       </section>
     </>
