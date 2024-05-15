e94d0161a810d3b626904496cdc7188210fa8119
TriBhaskar <panthribhaskar@gmail.com>
2024-05-15 21:55:34 +0530

    Update styles in Content and Layout components

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -100,7 +100,7 @@ export default function Content() {
               </div>
             </li>
             <li className="mb-12">
-              <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-50">
+              <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
                 <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
                 <header
                   className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2"
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,15 @@
-import bgImg from "../assets/stacked-peaks-haikei.svg";
+import bgImg from "../assets/bgmountain.svg";
 import Content from "./Content";
 import Profile from "./Profile";
 export default function Layout() {
   return (
     <>
       <section
-        className="min-h-screen bg-cover bg-center bg-no-repeat bg-blend-color-dodge"
-        style={{ backgroundImage: `url(${bgImg})` }}
+        className="min-h-screen bg-[#111827] bg-cover bg-center bg-no-repeat bg-blend-color-dodge"
+        // style={{ backgroundImage: `url(${bgImg})` }}
       >
-        <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md: px-12 md: py-20 lg: px-24 lg:py-0">
-          <div className="lg:flex lg:justify-between lg:gap-4 backdrop-blur-3xl">
+        <div className="mx-auto min-h-screen backdrop-blur-md max-w-screen-xl px-6 py-12 md: px-12 md: py-20 lg: px-24 lg:py-0">
+          <div className="lg:flex lg:justify-between lg:gap-4">
             <Profile />
             <Content />
           </div>
