2f80279b119ecf73d9757511c1d3d671abd25013
TriBhaskar <panthribhaskar@gmail.com>
2024-05-25 15:23:36 +0530

    feat: Add new project images

--- src/assets/Images.js
+++ src/assets/Images.js
@@ -3,13 +3,38 @@ import events2 from "./ProjectImages/events2.png";
 import events3 from "./ProjectImages/events3.png";
 import events4 from "./ProjectImages/events4.png";
 import events5 from "./ProjectImages/events5.png";
-
+import cloths from "./ProjectImages/cloths.png";
+import countdowngame from "./ProjectImages/CountdownGame.png";
+import countdowngame2 from "./ProjectImages/CountdownGame2.png";
+import placepicker from "./ProjectImages/placepicker.png";
+import projectmanagement from "./ProjectImages/Projectmangement1.png";
+import projectmanagement2 from "./ProjectImages/Projectmangement2.png";
+import reduxcart from "./ProjectImages/reduxCart.png";
+import shikafood from "./ProjectImages/Shikafood.png";
+import shikafood1 from "./ProjectImages/Shikafood1.png";
+import shikafood2 from "./ProjectImages/Shikafood2.png";
+import tictacto from "./ProjectImages/tictacto.png";
+import mobiPharama1 from "./ProjectImages/MobiPharam1.jpg";
+import ePharma from "./ProjectImages/Epharma.png";
 const Images = {
   events1,
   events2,
   events3,
   events4,
   events5,
+  cloths,
+  countdowngame,
+  countdowngame2,
+  placepicker,
+  projectmanagement,
+  projectmanagement2,
+  reduxcart,
+  shikafood,
+  shikafood1,
+  shikafood2,
+  tictacto,
+  mobiPharama1,
+  ePharma,
 };
 export default Images;
 // Path: src/components/Content.jsx
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,5 @@
 import projImg from "../assets/Waiau.png";
+import Images from "../assets/Images";
 export default function Project() {
   return (
     <section
@@ -20,7 +21,7 @@ export default function Project() {
                 <h3>
                   <a
                     className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-color1 focus-visible:text-color1  group/link text-base"
-                    href="https://www.newline.co/courses/build-a-spotify-connected-app"
+                    // href=""
                     target="_blank"
                     rel="noreferrer noopener"
                     aria-label="Build a Spotify Connected App (opens in a new tab)"
@@ -57,14 +58,69 @@ export default function Project() {
               <img
                 alt="Build a Spotify Connected App Newline course marketing card"
                 loading="lazy"
-                width="200"
-                height="48"
+                // width="200"
+                // height="58"
                 decoding="async"
                 data-nimg="1"
                 className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
                 style={{ color: "transparent" }}
                 // srcSet="../assets/Waiau.png&amp;w=256&amp;q=75 1x, ../assets/Waiau.png&amp;w=640&amp;q=75 2x"
-                src={projImg}
+                src={Images.events1}
+              ></img>
+            </div>
+          </li>
+          <li className="mb-12">
+            <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+              <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+              <div className="z-10 sm:order-2 sm:col-span-6">
+                <h3>
+                  <a
+                    className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-color1 focus-visible:text-color1  group/link text-base"
+                    // href="#"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    aria-label="Build a Spotify Connected App (opens in a new tab)"
+                  >
+                    <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                    <span>
+                      Build a Spotify Connected
+                      <span className="inline-block">
+                        App
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                          aria-hidden="true"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                            clipRule="evenodd"
+                          ></path>
+                        </svg>
+                      </span>
+                    </span>
+                  </a>
+                </h3>
+                <p className="mt-2 text-sm leading-normal">
+                  Video course that teaches how to build a web app with the
+                  Spotify Web API. Topics covered include the principles of REST
+                  APIs, user auth flows, Node, Express, React, Styled
+                  Components, and more.
+                </p>
+              </div>
+              <img
+                alt="Build a Spotify Connected App Newline course marketing card"
+                loading="lazy"
+                // width="200"
+                // height="58"
+                decoding="async"
+                data-nimg="1"
+                className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+                style={{ color: "transparent" }}
+                // srcSet="../assets/Waiau.png&amp;w=256&amp;q=75 1x, ../assets/Waiau.png&amp;w=640&amp;q=75 2x"
+                src={Images.mobiPharama1}
               ></img>
             </div>
           </li>
@@ -75,7 +131,7 @@ export default function Project() {
                 <h3>
                   <a
                     className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-color1 focus-visible:text-color1 group/link text-base"
-                    href="https://www.newline.co/courses/build-a-spotify-connected-app"
+                    // href="#"
                     target="_blank"
                     rel="noreferrer noopener"
                     aria-label="Build a Spotify Connected App (opens in a new tab)"
@@ -119,7 +175,62 @@ export default function Project() {
                 className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
                 style={{ color: "transparent" }}
                 // srcSet="../assets/Waiau.png&amp;w=256&amp;q=75 1x, ../assets/Waiau.png&amp;w=640&amp;q=75 2x"
-                src={projImg}
+                src={Images.shikafood}
+              ></img>
+            </div>
+          </li>
+          <li className="mb-12">
+            <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+              <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+              <div className="z-10 sm:order-2 sm:col-span-6">
+                <h3>
+                  <a
+                    className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-color1 focus-visible:text-color1 group/link text-base"
+                    // href="#"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    aria-label="Build a Spotify Connected App (opens in a new tab)"
+                  >
+                    <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                    <span>
+                      Build a Spotify Connected
+                      <span className="inline-block">
+                        App
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                          aria-hidden="true"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                            clipRule="evenodd"
+                          ></path>
+                        </svg>
+                      </span>
+                    </span>
+                  </a>
+                </h3>
+                <p className="mt-2 text-sm leading-normal">
+                  Video course that teaches how to build a web app with the
+                  Spotify Web API. Topics covered include the principles of REST
+                  APIs, user auth flows, Node, Express, React, Styled
+                  Components, and more.
+                </p>
+              </div>
+              <img
+                alt="Build a Spotify Connected App Newline course marketing card"
+                loading="lazy"
+                width="200"
+                height="48"
+                decoding="async"
+                data-nimg="1"
+                className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+                style={{ color: "transparent" }}
+                // srcSet="../assets/Waiau.png&amp;w=256&amp;q=75 1x, ../assets/Waiau.png&amp;w=640&amp;q=75 2x"
+                src={Images.ePharma}
               ></img>
             </div>
           </li>
