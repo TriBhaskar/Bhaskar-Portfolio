43af54f3e1a52df3ddfffe7d494b06f534c778a1
TriBhaskar <panthribhaskar@gmail.com>
2024-05-25 13:41:14 +0530

    Added images

---
+++ src/assets/Images.js
@@ -0,0 +1,15 @@
+import events1 from "./ProjectImages/events1.png";
+import events2 from "./ProjectImages/events2.png";
+import events3 from "./ProjectImages/events3.png";
+import events4 from "./ProjectImages/events4.png";
+import events5 from "./ProjectImages/events5.png";
+
+const Images = {
+  events1,
+  events2,
+  events3,
+  events4,
+  events5,
+};
+export default Images;
+// Path: src/components/Content.jsx
